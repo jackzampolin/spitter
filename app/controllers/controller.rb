1f6291cfcac5805292b6e7ee5ccfa2fdeca1be75
Jack Zampolin <jack.zampolin@gmail.com>
2015-04-30 12:41:42 -0700

    need to build out DELETE and PUT routes for /spits/:id

--- app/controllers/controller.rb
+++ app/controllers/controller.rb
@@ -1,5 +1,17 @@
 require 'bcrypt'
 
 get '/' do
+  @spits = Spit.first(50).sort_by &:created_at
   erb :index
+end
+
+get '/spits/:id' do
+  @spit = Spit.where(id: params[:id]).first
+  erb :'spits/spit_page'
+end
+
+delete '/spits/:id' do
+end
+
+put '/spits/:id' do
 end
